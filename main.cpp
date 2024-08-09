906975e9648f270d3c717c0b1119fe138900c11d
SDW1014 <apif730@gmail.com>
2024-08-10 08:36:40 +0900

    [feat] 변수 a와 b를 만들고, 각각 a에는 7을 b에는 2를 넣어

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-// std를 사용하는 대신, 각 함수와 변수마다 std::를 사용하는 것이 더 좋습니다. 
-// 이렇게 하면 코드의 가독성이 높아지고, 어떤 네임스페이스에서 오는 것인지 명확해집니다.
+// 변수 두개를 만들고 더한다. 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a = 7;
+	int b = 2;
+
+	std::cout << "a + b = " << a + b << std::endl;
+	std::cout << "a - b = " << a - b << std::endl;
+
 	return 0;
 }
