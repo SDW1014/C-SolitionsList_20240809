6eea18037364cecb05725f212dbb9417940b9b9e
SDW1014 <apif730@gmail.com>
2024-08-09 17:58:44 +0900

    [feat] 연습1 :  1주일동안 얼마나 운동을 했는가에 대한 간단한 알고리즘

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
-// std를 사용하는 대신, 각 함수와 변수마다 std::를 사용하는 것이 더 좋습니다. 
-// 이렇게 하면 코드의 가독성이 높아지고, 어떤 네임스페이스에서 오는 것인지 명확해집니다.
+//1주일이 곧 7일이기에, 절반 이상 운동을 하였으면 칭찬하는 알고리즘을 짜봐라 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+    int weekExcerciseDay = 0;
+    std::wcout << "당신은 이번주 얼마나 운동을 하셨습니까? : " << std::endl;
+
+    std::cin >> weekExcerciseDay;
+
+    if(weekExcerciseDay >= 7/2 + 1)
+    {
+        std::wcout << "이번주도 수고하셨습니다." << std::endl;
+        return 0;
+    }
+    std::wcout << "다음주는 분발 하셨으면 좋겠습니다." << std::endl;
+    return 0;
 }
