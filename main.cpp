fcff14635118eb4360d09e09301d9e07cf96c02a
SDW1014 <apif730@gmail.com>
2024-08-09 17:42:41 +0900

    [feat] main.cpp 추가, (Hello,World 기본 기능) : Using namespace를 사용하지 않는걸 권장

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// std를 사용하는 대신, 각 함수와 변수마다 std::를 사용하는 것이 더 좋습니다. 
+// 이렇게 하면 코드의 가독성이 높아지고, 어떤 네임스페이스에서 오는 것인지 명확해집니다.
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
