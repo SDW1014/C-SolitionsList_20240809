2870388cf3a7f4ff445da406c05b3cc4b8838784
SDW1014 <apif730@gmail.com>
2024-08-10 08:48:40 +0900

    [feat] Hello, world를 출력하는 기본적 코드

--- main.cpp
+++ main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 
-// 변수 a를 선언해주세요.
-// 변수 a에 8을 넣어주세요.
-// a를 5번 곱한 값, a의 5승의 값을 출력해 주세요.
-
-int fivesqure(int a)
-{
-	return a * a * a * a * a;
-}
+//시작의 코드 
 
 int main()
 {
-	int a = 8;
-
-	std::cout << fivesqure(a) << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
 }
