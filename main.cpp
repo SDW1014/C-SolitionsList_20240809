d31d152e9cd5d377819f83cc8cfe1842f18c4193
SDW1014 <apif730@gmail.com>
2024-08-10 08:42:28 +0900

    [feat] ((a+b)*c)/d 의 값을 출력

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
-// 시작의 코드 
+// 변수 a, b, c, d 를 선언해 주세요.
+// 변수값 3, 5, 3, 4를 순서대로 아래와 같이 넣어주세요.
+// 수식 ((a+b)*c)/d 의 값을 출력해 주세요.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a = 3, b = 5, c = 3, d = 4;
+	std::cout << ((a+b) * c) / d << std::endl;
 	return 0;
 }
