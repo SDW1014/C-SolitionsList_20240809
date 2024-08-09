830ebb42e0ce32aa51900b41dc58e63812049952
SDW1014 <apif730@gmail.com>
2024-08-10 08:47:43 +0900

    [feat] a를 5번 곱한 값, a의 5승의 값을 출력

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
-// 시작의 코드 
+// 변수 a를 선언해주세요.
+// 변수 a에 8을 넣어주세요.
+// a를 5번 곱한 값, a의 5승의 값을 출력해 주세요.
+
+int fivesqure(int a)
+{
+	return a * a * a * a * a;
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a = 8;
+
+	std::cout << fivesqure(a) << std::endl;
+
 	return 0;
 }
