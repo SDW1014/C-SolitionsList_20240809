7d91e942c4d6ac1d969c182a3fa64347298d1934
SDW1014 <apif730@gmail.com>
2024-08-10 07:59:40 +0900

    [feat] 초기화 할당 프린트 예제

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-// std를 사용하는 대신, 각 함수와 변수마다 std::를 사용하는 것이 더 좋습니다. 
-// 이렇게 하면 코드의 가독성이 높아지고, 어떤 네임스페이스에서 오는 것인지 명확해집니다.
+// 기존 항목에서, 5를 출력해야함
+// int 를 만들어서 5를 출력한다. 
+// 초기화도 해야하고 할당도 해야한다. 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int x = 5;
+	std::cout << x << std::endl;
 	return 0;
 }
