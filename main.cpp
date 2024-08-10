a8a71b89d67b5862b3b84c840213ebdc90a3865d
SDW1014 <apif730@gmail.com>
2024-08-10 16:19:50 +0900

    [feat] 숫자 4개 a, b, c, d를 입력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
-// 기본
+// 숫자 4개 a, b, c, d를 입력받아주세요
+// a + b를 먼저 출력하고
+// c + d를 출력 해주세요
+// 그리고 a + b + c + d의 값을 출력 해 주세요
+
+// [입력 예시]
+// 2 4 5 10
+
+// [출력 예시]
+// a + b = 6
+// c + d = 15
+// ALL SUM = 21
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b, c, d;
+	
+	std::cin >> a >> b >> c >> d;
+
+	std::cout << "a + b = " << a + b << std::endl;
+	std::cout << "c + d = " << c + d << std::endl;
+	std::cout << "ALL SUM = " << a + b + c + d << std::endl;
 
 	return 0;
 }
