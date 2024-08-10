475333c203bab196e6168e4c4398502b12068021
SDW1014 <apif730@gmail.com>
2024-08-10 13:53:55 +0900

    [feat] 차이 : [앞 숫자] - [뒷 숫자]

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-// 기본
+//문제 5번
+//숫자 2개를 입력받으세요
+//그리고 그 차를 구해서 출력 해 주세요
+//차이 : [앞 숫자] - [뒷 숫자]
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-
+	int a, b;
+	
+	std::cin >> a >> b;
+	
+	std::cout << a << " - " << b << " = " << a - b << std::endl;
 	return 0;
 }
