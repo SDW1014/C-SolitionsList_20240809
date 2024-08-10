bb8c788142617762dc78a6c3f2f697f2ede29428
SDW1014 <apif730@gmail.com>
2024-08-10 18:47:56 +0900

    [feat] for문을 이용해 "#"을 총 10회 출력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
-// 기본
+ // Start of Selection
+ // 문제 11번
+ // 위와 같이 코딩하면 #이 3회 출력 됩니다.
+ // 이것을 소스코드로 표현하면 아래와 같이 작성할 수 있습니다.
+ // **위를 참고하여**
+ // **for문을 이용해 #을 총 10회 출력** 하는 소스코드를 작성해주세요.**(입력없음)**
+ // 출력 예시
+ // ##########
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-
+	for(int i = 0 ; i < 10; ++i)
+		std::cout << "#";
+	std::cout << std::endl;
 	return 0;
 }
