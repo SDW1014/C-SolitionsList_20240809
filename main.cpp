7a1cafc0203450216838b8accebf40c48b758d64
SDW1014 <apif730@gmail.com>
2024-08-10 16:23:09 +0900

    [feat] 입력한 수 3개가 전부 같으면 만세

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
-// 기본
+// 숫자 3개를 변수 a, b, c 에 입력 받기
+// 세 숫자가 같으면 "만세" 출력
+// 세 숫자가 같지 않으면 아무것도 출력하지 않음
+// 입력 예시: 1 1 1
+// 출력 예시: 만세
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b, c;
+
+	std::cin >> a >> b >> c;
+
+	if(a == b && b == c)
+		std::cout << "만세" << std::endl;
 
 	return 0;
 }
