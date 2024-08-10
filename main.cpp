b43e46832ea5a769d5e5beea107199a034e92409
SDW1014 <apif730@gmail.com>
2024-08-10 16:07:45 +0900

    [feat] 같습니다 다릅니다 문제

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 
-// 기본
+ // Start of Selection
+ // 숫자 두개를 입력 받아 주세요.
+ // 만약 두 숫자가 같다면
+ // **같습니다**를 출력해 주세요.
+ // 다르다면
+ // **다릅니다** 를 출력 해 주세요
+ // 입력 예시: 5 5
+ // 출력 예시: 같습니
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b;
+	std::cin >> a >> b;
 
+	if(a == b)
+	{
+		std::cout << "같습니다." << std::endl;
+		return 0;
+	}
+
+	std::cout << "다릅니다." << std::endl;
+	
 	return 0;
 }
