2207c704c9e24756b72bbb4f57064369cb04cc63
SDW1014 <apif730@gmail.com>
2024-08-10 16:02:13 +0900

    [feat] 행운의 수와 소소한 수 문제

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
-// 기본
+// 문제 12번
+// 숫자 3개를 입력 받으세요.
+// 세 수의 합이 세 수의 곱보다 같거나 크다면 "행운의 수" 출력
+// 아니면 "소소한 수" 출력
+// 입력 예시: 1 2 3
+// 출력 예시: 행운의 수
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b, c;
+
+	std::cin >> a >> b >> c;
+
+	if(a + b + c >= a * b * c)
+		std::cout << "Lucky Number" << std::endl;
+	else
+		std::cout << "Small Number" << std::endl;
 
 	return 0;
 }
