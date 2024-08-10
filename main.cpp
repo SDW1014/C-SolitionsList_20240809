409ab075f554ea863ba35830213bcef4b85fca53
SDW1014 <apif730@gmail.com>
2024-08-10 14:33:03 +0900

    [feat] 숫자 2개를 입력 받고, 둘 중 큰수를 4번 출력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
-// 기본
+ // 문제 8번
+ // 숫자 2개를 입력 받고, 둘 중 큰수를 4번 출력 해주세요.
+ // ex)
+ // [입력] : 3 5
+ // [출력] : 5555
+
+void printContinue4(int num)
+{
+	for(int i = 0; i < 5; ++i)
+		std::cout << num;
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b;
+	
+	std::cin >> a >> b;
+
+	if(a > b)
+		printContinue4(a);
+	else
+		printContinue4(b);
 
 	return 0;
 }
