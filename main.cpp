348a780b7eb59e0ad193e10c77e4d05b45046b16
SDW1014 <apif730@gmail.com>
2024-08-10 18:43:16 +0900

    [feat] 숫자 7개를 입력받고, 마지막 4개를 출력하는 프로그램을 작성 : (배열을 사용해 봄)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
-// 기본
+/*
+💡 **문제 10번**
+
+숫자 7개를 입력받고, 마지막 4개를 출력하는 프로그램을 작성하세요.
+
+[힌트]
+띄어쓰기 출력 방법: 
+cout << a << " " << b << " " << c;
+
+## 입력 예시
+7 6 5 4 3 2 1
+
+## 출력 예시
+4 3 2 1
+*/
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int numbers[7];
+
+	for(int i = 0 ; i < 7; ++i)
+		std::cin >> numbers[i];
+
+	for(int i = 3; i < 7; ++i)
+		std::cout << numbers[i] << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
