ce760a7a2d5c96f69c8443e973b757db77bca2cf
SDW1014 <apif730@gmail.com>
2024-08-10 16:05:48 +0900

    [feat] WWW."LJJ"CODING.CO.KR 를 출력 (\") 를 연습하는 문제

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-// 기본
+// 문제 1번
+// 다음과 같이 출력 해 주세요
+// 힌트 : 쌍따옴표는 **\"** 를 사용해서 출력하시면 됩니다.
+// WWW."LJJ"CODING.CO.KR
+// ---
+// 출력 예시
+// WWW."LJJ"CODING.CO.KR
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "WWW.\"LJJ\"CODING.CO.KR";
 
 	return 0;
 }
