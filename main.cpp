1fcc8eef5382d3be607da08eb2a3f07422e4c2f6
SDW1014 <apif730@gmail.com>
2024-08-10 12:09:14 +0900

    [feat] 다시 기본으로 변경

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
 
-// 문제 3번
-// 변수 q, w, e를 선언하고 아래와 같이 값을 채워주세요.
-// 값은 다음과 같습니다:
-// q = 1
-// w = 2
-// e = 3
-// 출력 예시:
-// #q=1,1,1
-// #w=2,2,2
-// #e=3,3,3
+// 기본
 
 int main()
 {
-	int q = 1, w = 2, e = 3;
-
-	std::cout << "#q=" << q << "," << q << "," << q << std::endl;
-	std::cout << "#w=" << w << "," << w << "," << w << std::endl;
-	std::cout << "#e=" << e << "," << e << "," << e << std::endl;	
+	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
 }
