145443ed96f01dd795f9307761e5cd66bea32700
SDW1014 <apif730@gmail.com>
2024-08-10 14:36:51 +0900

    [feat] 숫자 1개를 변수 a에 입력 받고, 다음 규칙에 맞게 출력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
-// 기본
+// 문제 9번
+// 숫자 1개를 변수 a에 입력 받고, 다음 규칙에 맞게 출력하세요.
+// 5 입력함
+// a++을 수행하면 6이 됩니다
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a;
+	std::cin >> a;
+
+	std::cout << a << "를 입력하셨습니다." << std::endl;
+	std::cout << "a++을 수행하면 " << a+1 << "이 됩니다." << std::endl;
 
 	return 0;
 }
