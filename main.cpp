de717ce806920ff711a9d7f90d18e9139b104667
SDW1014 <apif730@gmail.com>
2024-08-10 12:04:51 +0900

    [feat] 1234->0 으로 출력하는 문제

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 
-//시작의 코드 
+// 💡 **문제 1번**
+// 아래의 그림과 같이 출력 해 주세요
+// 5앞에는 띄어쓰기 한칸
+// 8앞에는 띄어쓰기 두칸
+// 0앞에는 띄어쓰기 세칸이 있습니다.
+// 
+// ## 출력 예시
+// 1234
+// 567
+// 89
+// 0
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-
+	int index = 1;
+	for(int i = 0 ; i < 4; i++)
+	{
+		for(int j = 0; j < 4-i; j++)
+		{
+			std::cout << index%10;
+			index++;
+		}
+		std::cout << std::endl;
+	}
+	
 	return 0;
 }
