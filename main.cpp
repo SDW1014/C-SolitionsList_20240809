6d342535443b5a195b59dc6ca8a795d49cc262ab
SDW1014 <apif730@gmail.com>
2024-08-10 11:40:00 +0900

    [feat] a를 12로 만들어라

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
-//시작의 코드 
+// 변수 a를 선언해 주세요.
+// 변수값 8을 a에 넣어주세요.
+// a = a + 1, a++ 를 적절히 이용하여
+// a를 12로 만들어 주시면 됩니다.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a  = 8; 
+	a = a + 1;
+	a++;
+	a = a + 1;
+	a++;
+	
+	std::cout << "a의 값은 : " << a << std::endl;
 
 	return 0;
 }
