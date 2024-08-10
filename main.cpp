cebaa716f0758f667890dd2cb24613263b3e8e92
SDW1014 <apif730@gmail.com>
2024-08-10 14:28:19 +0900

    [feat] **a가 b보다 크다** OR **b가 a보다 같거나 크다**

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 
-// 기본
+// 문제 7번
+// 변수 a, b에 숫자를 입력 받으세요.
+// 만약 변수 a가 b 보다 크다면
+// **a가 b보다 크다**
+// 를 출력 해 주세요.
+// 만약 그렇지 않다면
+// **b가 a보다 같거나 크다**
+// 를 출력 해 주세요.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b;
+	std::cin >> a >> b;
+	if(a > b)
+	{
+		std::cout << "a가 b보다 크다." << std::endl;
+	}
+	else
+	{
+		std::cout << "b가 a보다 같거나 크다." << std::endl; 
+	}
 
 	return 0;
 }
