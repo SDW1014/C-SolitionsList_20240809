a291dc663cd821a7fb2248220923487230f4e3f7
SDW1014 <apif730@gmail.com>
2024-08-10 16:28:38 +0900

    [feat] 숫자 4개를 변수 b1, b2, b3, b4 에 입력하고 가장 큰 수 이름을 출력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 
-// 기본
+// 숫자 4개를 변수 b1, b2, b3, b4 에 입력 받아주세요.
+// 입력 예시: 9 4 5 3
+// 출력 예시: b1이 가장 크다
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int b1, b2, b3, b4;
+	
+	std::cin >> b1 >> b2 >> b3 >> b4;
+
+	if(b1 > b2 && b1 > b3 && b1 > b4)
+		std::cout << "b1이 가장 크다" << std::endl;
+	else if(b2 > b1 && b2 > b3 && b2 > b4)
+		std::cout << "b2가 가장 크다" << std::endl;
+	else if(b3 > b1 && b3 > b2 && b3 > b4)
+		std::cout << "b3가 가장 크다" << std::endl;
+	else if(b4 > b1 && b4 > b2 && b4 > b3)
+		std::cout << "b4가 가장 크다" << std::endl;
 
 	return 0;
 }
