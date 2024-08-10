b1bcb4990178b0e5e9aac1287e9bdd04ff361dd8
SDW1014 <apif730@gmail.com>
2024-08-10 15:44:05 +0900

    [feat] 3보다 크면 ++ 나머지는 --

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
-// 기본
+// 문제 10번
+// 숫자 1개를 변수 input에 입력 받으세요.
+// 그 숫자가 3보다 크면 1을 더하고 출력 해주세요. (input++;을 활용하세요)
+// 그렇지 않으면 1을 빼고 출력 해주세요. (input--;를 활용 하세요)
+// [문제] 5 => 6
+// [문제] 1 => 0
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a;
 
+	std::cin >> a;
+
+	if(a > 3)
+		a++;
+	else
+		a--;
+
+	std::cout << a << std::endl;
+	
 	return 0;
 }
