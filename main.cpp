09794806ba08fc83022f696650fb267536222bfb
SDW1014 <apif730@gmail.com>
2024-08-10 13:45:38 +0900

    [feat] 입력받은 숫자를 그대로 출력 하고, 뒤에 "입력하셨군요"라는 글씨를 붙여서 출력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
-// 기본
+//숫자 하나를 입력받고 출력하세요
+//입력받은 숫자를 그대로 출력 하고, 뒤에 "입력하셨군요"라는 글씨를 붙여서 출력하시면 됩니다.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int input;
+	
+	std::cout << "수를 입력하십시오 : ";
+	std::cin >> input;
+
+	std::cout << "당신은 수를 입력하셨습니다." << std::endl;
 
 	return 0;
 }
