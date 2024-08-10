23acb8150578e4eac20f34a8baf1c29e0c135148
SDW1014 <apif730@gmail.com>
2024-08-10 13:47:38 +0900

    [feat] 그 숫자를 세번 출력

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
-// 기본
+//숫자를 하나만 입력 받아주세요.
+//그 숫자를 세번 출력 해 주시면 됩니다.
+//[참고]
+// cin과 cout만 이용해서 풀어주세요.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int input;
+	std::cout << "숫자를 입력하십시오 : ";
+	std::cin >> input;
+
+	std::cout << input << " " << input << " " << input << std::endl;
 
 	return 0;
 }
