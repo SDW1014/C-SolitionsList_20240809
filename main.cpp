29cabdf6fe2052db7426736bacf4a4959aefe3c8
SDW1014 <apif730@gmail.com>
2024-08-11 23:47:22 +0900

    [feat] // 문제 8번 // 숫자 하나를 입력 받고 "##" 을 입력받은 숫자만큼 출력

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
-
+// 문제 8번
+// 숫자 하나를 입력 받고 "##" 을 입력받은 숫자만큼 출력 해주세요.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a;
+	
+	std::cin >> a;
+
+	for(int i = 0; i < a; ++i)
+		std::cout << "##" << std::endl;
 
 	return 0;
 }
