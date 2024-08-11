5291f461b3a1874a0be16d127e52805dc5cb96d4
SDW1014 <apif730@gmail.com>
2024-08-12 00:52:51 +0900

    [feat] // 문제 11 // for문을 사용해 아래와 같이 출력되도록 소스코드

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 문제 11
+// for문을 사용해 아래와 같이 출력되도록 소스코드를 작성해 주세요
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "시작" << std::endl;
+
+	for(int i = 1; i <= 5; ++i)
+		std::cout << i;
+	
+	std::cout << std::endl << "종료" << std::endl;
 
 	return 0;
 }
