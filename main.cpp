55a893e8bb40a6cff9b62025c9b1b0fcaac6340c
SDW1014 <apif730@gmail.com>
2024-08-12 00:05:20 +0900

    [feat] // 변수 a, b를 만들고 숫자를 각각 입력 // 그리고 a부터 b까지의 숫자를 출력

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
+// 변수 a, b를 만들고 숫자를 각각 입력 받아주세요.
+// 그리고 a부터 b까지의 숫자를 출력 해주세요.
+// ex1) 입력: 3 5 출력: 345
+// ex2) 입력: 2 6 출력: 23456
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a,b;
+
+	std::cin >> a >> b;
+
+	for(int i = a; i <= b; ++i)
+		std::cout << i; 
+	std::cout << std::endl;
 
 	return 0;
 }
