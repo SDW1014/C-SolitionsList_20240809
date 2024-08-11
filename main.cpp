1c5480bc532c99e933ce990eb1168ddddd527b53
SDW1014 <apif730@gmail.com>
2024-08-12 00:22:05 +0900

    [feat] // 문제 2번 // 숫자 입력에 따라 출력하는 프로그램 // 입력 예시: 3 -> 출력: 333, 444, 555 // 입력 예시: 7 -> 출력: 777, 888, 999

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 문제 2번
+// 숫자 입력에 따라 출력하는 프로그램
+// 입력 예시: 3 -> 출력: 333, 444, 555
+// 입력 예시: 7 -> 출력: 777, 888, 999
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a; 
+	
+	std::cin >> a;
+
+	for(int i = 0 ; i < 3; ++i)
+	{
+		for(int j = 0; j < 3; ++j)
+			std::cout << a + i;
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
