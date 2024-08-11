bf5a79efb80b56d3f5de96c6012881413d5f24c3
SDW1014 <apif730@gmail.com>
2024-08-12 00:57:17 +0900

    [feat] // 문제 13번 // for문을 이용해 아래와 같이 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 문제 13번
+// for문을 이용해 아래와 같이 출력
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	for(int i = 1 ; i <= 5; i++)
+		std::cout << i << "번go!!" << std::endl;
 
 	return 0;
 }
