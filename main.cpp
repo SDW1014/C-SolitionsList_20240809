5acf96398afdacea91bc3b7debfe1f205e9daa90
SDW1014 <apif730@gmail.com>
2024-08-11 23:15:18 +0900

    [feat] // 숫자 1개를 입력 // 만약 그 숫자가 10보다 같거나 크면 // "WOW"를 출력 // 그렇지 않으면 // "OMG"를 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
-// [2024-08-10]
-// 제목 : LV3으로 넘어간다.
+// [2024-08-11]
+// 숫자 1개를 입력 받으세요
+// 만약 그 숫자가 10보다 같거나 크면
+// "WOW"를 출력하세요
+// 그렇지 않으면
+// "OMG"를 출력하세요
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a;
+	
+	std::cin >> a;
+	
+	if(a >= 10)
+	{
+		std::cout << "WOW" << std::endl;
+		return 0;
+	}
 
+	std::cout << "OMG" << std::endl;
 	return 0;
 }
