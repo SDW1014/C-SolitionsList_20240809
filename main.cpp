f5d2a9838e5ef393d6821c7b3827cfc8e4d67b1c
SDW1014 <apif730@gmail.com>
2024-08-12 00:13:14 +0900

    [feat] // 문제 14번 // 숫자 한 개를 입력받아 Count Down을 출력 // 입력받은 숫자부터 0까지 감소하는 과정을 출력 // 마지막에는 "발사"를 출력

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
+// 문제 14번 
+// 숫자 한 개를 입력받아 Count Down을 출력합니다.
+// 입력받은 숫자부터 0까지 감소하는 과정을 출력하고,
+// 마지막에는 "발사"를 출력합니다.
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int CountNumber; 
+
+	std::cin >> CountNumber;
+
+	for(int i = CountNumber; i >= 0; --i)
+		std::cout << i << std::endl;
+	std::cout << "발사" << std::endl;
 
 	return 0;
 }
