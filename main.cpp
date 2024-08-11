b972217edf037b788c0708e553859070af855f72
SDW1014 <apif730@gmail.com>
2024-08-12 00:48:43 +0900

    [feat] // 문제 10 // 숫자 하나를 입력 받아 홀수 출력 // 규칙: 입력한 숫자까지의 홀수를 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 문제 10
+// 숫자 하나를 입력 받아 홀수 출력하기.
+// 규칙: 입력한 숫자까지의 홀수를 출력한다.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a; 
 
+	std::cin >> a;
+
+	for(int i = 0; i < a * 2; ++i)
+		if(i % 2)
+			std::cout << i << " ";
+	std::cout << std::endl;
+	
 	return 0;
 }
