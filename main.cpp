a93ebe397cdc296af62c9c7199f584e3a9f70a19
SDW1014 <apif730@gmail.com>
2024-08-12 00:45:54 +0900

    [feat] // 문제 9번 // 1중 for문으로 아래와 같이 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 문제 9번
+// 1중 for문으로 아래와 같이 출력 하세요
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int nine = 9, six = 6;
+
+	for(int i = 0; i < 4; ++i)
+		std::cout << nine - i << " " << six - i << std::endl;
 
 	return 0;
 }
