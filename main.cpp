57690619ba15d9b4274bac6a474de4fe3906f429
SDW1014 <apif730@gmail.com>
2024-08-12 00:55:47 +0900

    [feat] // 제목 : 숫자 입력 및 출력 // 입력: 숫자 하나를 입력 // 출력: 입력받은 숫자만큼 "##"과 "@@"을 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 제목 : 숫자 입력 및 출력
+// 입력: 숫자 하나를 입력 받고
+// 출력: 입력받은 숫자만큼 "##"과 "@@"을 출력 해 주세요.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a; 
 
+	std::cin >> a;
+
+	for(int i = 0 ; i < a; ++i)
+		std::cout << "##" << std::endl << "@@" << std::endl;
+	
 	return 0;
 }
