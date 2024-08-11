255cf02ad7f181b258cc2d12dcaff80ebfbd0a09
SDW1014 <apif730@gmail.com>
2024-08-12 00:29:29 +0900

    [feat] // 문제 4번: 숫자 하나를 입력 받아주세요. // 아래의 규칙을 보고 for문을 사용해 값을 출력 해주세요.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 문제 4번: 숫자 하나를 입력 받아주세요.
+// 아래의 규칙을 보고 for문을 사용해 값을 출력 해주세요.
+// ex1) 입력: 3 -> 출력: 5 6 7
+// ex2) 입력: 5 -> 출력: 7 8 9
+// ex3) 입력: 9 -> 출력: 11 12 13
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a; 
+
+	std::cin >> a;
+
+	for(int i = 0; i < 3; ++i)
+		std::cout << a + i + 2 << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
