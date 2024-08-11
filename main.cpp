6b44b36a287c0ed79a9d995a5f16fb8e79431260
SDW1014 <apif730@gmail.com>
2024-08-11 23:41:27 +0900

    [feat] // 문제 6번 // 숫자 2개를 입력 // 첫번째 숫자부터 두번째 숫자 값 까지 출력

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
+// 문제 6번
+// 숫자 2개를 입력 받으세요 (두번째 숫자는 첫번째 숫자보다 큽니다)
+// 첫번째 숫자부터 두번째 숫자 값 까지 출력 해 주세요
+// for (x=시작값; x<=끝값; x++) { /* ... */ }
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b;
+
+	std::cin >> a >> b;
+
+	for(int i = a; i <= b; ++i)
+		std::cout << i << std::endl;
 
 	return 0;
 }
