8828651bbb961da9515fb4f5e13686272fc358f5
SDW1014 <apif730@gmail.com>
2024-08-12 00:06:54 +0900

    [feat] // 문제 12번 // 숫자 1개를 입력 // 0부터 입력받은 숫자까지 출력 // 만약 3을 입력 // 0 1 2 3을 출력

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
+// 문제 12번
+// 숫자 1개를 입력받으세요
+// 0부터 입력받은 숫자까지 출력 해 주세요
+// 만약 3을 입력하셨으면
+// 0 1 2 3을 출력 하시면 됩니다.
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a;
+
+	std::cin >> a;
+
+	for(int i = 0 ; i <= a; ++i)
+		std::cout << i << std::endl;
 
 	return 0;
 }
