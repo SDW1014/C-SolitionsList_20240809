fec962fbe4252fc2e3d4d10623803a0b4013abb4
SDW1014 <apif730@gmail.com>
2024-08-11 23:49:42 +0900

    [feat] // 문제 9번 // 10 ~ 1까지 for문을 돌려 출력

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
-
+// 문제 9번 
+// 10 ~ 1까지 for문을 돌려 출력 해주세요
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	for(int i = 10; i > 0; --i)
+		std::cout << i << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
