637063c5b859f466b83dfe165f2baab99c6c082c
SDW1014 <apif730@gmail.com>
2024-08-11 23:22:49 +0900

    [feat] // 숫자 하나를 입력 받으세요 // 만약 그 숫자가 5 이거나 10이면 "**만세**" 출력 // 그렇지 않으면 "**재도전**" 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
+// 문제 3번
+// 숫자 하나를 입력 받으세요
+// 만약 그 숫자가 5 이거나 10이면 "**만세**" 출력
+// 그렇지 않으면 "**재도전**" 출력
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a; 
+	
+	std::cin >> a;
 
+	if(a == 5 || a == 10)
+	{
+		std::cout << "**만세**" << std::endl;
+		return 0;
+	}
+	
+	std::cout << "**재도전**" << std::endl;
 	return 0;
 }
