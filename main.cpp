c9f447053c8c17f761a8565fd5c2a87c3eca1b2b
SDW1014 <apif730@gmail.com>
2024-08-12 01:03:15 +0900

    [feat] // 문제 15번 // 숫자 하나를 입력 받고, 입력받은 값부터 0까지 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV3으로 넘어간다.
+// 문제 15번
+// 숫자 하나를 입력 받고, 입력받은 값부터 0까지 출력 해주세요.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a; 
+
+	std::cin >> a;
+
+	for(int i = a; i >= 0; --i)
+		std::cout << i;
+	std::cout << std::endl;
 
 	return 0;
 }
