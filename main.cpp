4daebe291b58416d6cf891329c0ad5ec30732dcc
SDW1014 <apif730@gmail.com>
2024-08-12 00:18:23 +0900

    [feat] // 문제 1번 // 3부터 8까지 출력해주세요.(for문 이용)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
-
+// 문제 1번
+// 3부터 8까지 출력해주세요.(for문 이용)
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	for(int i = 3; i <= 8; ++i)
+		std::cout << i << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
