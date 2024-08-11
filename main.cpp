3706523fd3f5df012e727ebd94781b61fbd1b6c9
SDW1014 <apif730@gmail.com>
2024-08-11 23:52:50 +0900

    [feat] // 문제 10번 // 숫자에 2를 곱하고, 20을 더하고, 다시 5로 나눈 값이 100과 **같지 않으면...

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
+// 문제 10번
+// 숫자 1개를 입력 받으세요
+// 그 숫자에 2를 곱하고, 20을 더하고, 다시 5로 나눈 값이 100과 **같지 않으면 (!=)**
+// "**Magic**"이라는 글씨를 출력 하세요.
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a; 
+	
+	std::cin >> a;
+
+	a = ((a*2) + 20)/5;
+
+	if(a != 100)
+		std::cout << "**Magic**" << std::endl;
 
 	return 0;
 }
