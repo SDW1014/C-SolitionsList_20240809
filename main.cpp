142d91ce5625f57cd87db60871d5db7453f62ac0
SDW1014 <apif730@gmail.com>
2024-08-12 00:10:07 +0900

    [feat] // 문제 13번 // 숫자 3개를 입력 // 만약 숫자 3개의 합이 10보다 크면, 숫자 3개를 곱한 숫자를 출력 // 그렇지 않으면 0을 출력

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 
 // [2024-08-11]
-// 제목 : LV3으로 넘어간다.
+// 문제 13번
+// 숫자 3개를 입력 받아주세요. 
+// 만약 숫자 3개의 합이 10보다 크면, 숫자 3개를 곱한 숫자를 출력 해 주세요. 
+// 그렇지 않으면 0을 출력 해 주세요. 
+// 예시: 1 2 10 을 입력받으면 숫자 합은 13이니까, 3개 숫자의 곱인 20을 출력하시면 됩니다.
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b, c;
 
+	std::cin >> a >> b >> c;
+
+	if(a + b + c > 10)
+	{
+		std::cout << a*b*c << std::endl;
+		return 0;
+	}
+	
+	std::cout << "0" << std::endl;
 	return 0;
 }
