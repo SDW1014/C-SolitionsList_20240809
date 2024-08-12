ed73f8c58524f31f6a178a9b20b11c4e596c24b6
SDW1014 <apif730@gmail.com>
2024-08-12 15:46:20 +0900

    [feat] // 문제 9번 // 제목 : 대발견, 중발견, 미발견 // A와 B가 존재하는지, 알아내는 프로그램

--- main.cpp
+++ main.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV4으로 넘어간다.
+// 문제 9번
+// 최대 5글자인 문장 4개를 입력받아 2차 배열에 저장합니다.
+// 입력받은 문장에서 알파벳 A와 B가 모두 존재하면 "대발견"을 출력합니다.
+// 알파벳 A 또는 B 중 하나만 존재하면 "중발견"을 출력합니다.
+// 알파벳 A와 B가 모두 존재하지 않으면 "미발견"을 출력합니다.
+//
+// 입력 예시:
+// SHOW
+// YOUR
+// JASON
+// DATA
+//
+// 출력 예시:
+// 중발견
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char arr[4][20];
+
+	for(int i = 0 ; i < 4; ++i)
+		std::cin >> arr[i];
+
+	bool a_exist = false, b_exist = false;
+
+	for(int i = 0 ; i < 4; ++i)
+	{
+		for(int j = 0; j < 20; ++j)
+		{
+			if(arr[i][j] == 'A')
+				a_exist = true;
+			else if(arr[i][j] == 'B')
+				b_exist = true;
+		}
+	}
+
+	if(a_exist && b_exist)
+		std::cout << "대발견" << std::endl;
+	else if(a_exist || b_exist)
+		std::cout << "중발견" << std::endl;
+	else
+		std::cout << "미발견" << std::endl;
 
 	return 0;
 }
