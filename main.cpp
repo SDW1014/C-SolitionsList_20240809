7a89b1f7017389e514404740fd813177c015fe0c
SDW1014 <apif730@gmail.com>
2024-08-12 16:31:10 +0900

    [feat] // 문제 14번 // 두 문장을 하나의 이차배열에 입력받아주세요 (최대 8글자) // 두 문장에서 같은 index에 있지만 다른 글자가 몇개인지 Counting 하여 출력 해 주세요 // 입력 예시: BackLog, BackBt // 출력 예시: 3
    // [tip] 함정이 살작 있음
    // 둘의 길이가 달랐을때는 잘못 비교하게됨 그걸 strLen으로 해결함

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV4으로 넘어간다.
+// 문제 14번
+// 두 문장을 하나의 이차배열에 입력받아주세요 (최대 8글자)
+// 두 문장에서 같은 index에 있지만 다른 글자가 몇개인지 Counting 하여 출력 해 주세요
+// 입력 예시: BackLog, BackBt
+// 출력 예시: 3
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char arr[2][8];
+
+	std::cin >> arr[0] >> arr[1];
+
+	int count = 0;
+
+	for(int i = 0 ; i < (strlen(arr[0]) < strlen(arr[1]) ? strlen(arr[0]) : strlen(arr[1])); ++i)
+		if(arr[0][i] != arr[1][i])
+			++count;
+			
+	count += strlen(arr[0]) < strlen(arr[1]) ? strlen(arr[1]) - strlen(arr[0]) : strlen(arr[0]) - strlen(arr[1]);
+
+	std::cout << count << std::endl;
 
 	return 0;
 }
