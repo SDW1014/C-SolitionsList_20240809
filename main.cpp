b061fc8a4049ae241fb65721555cb03f217070de
SDW1014 <apif730@gmail.com>
2024-08-12 14:18:03 +0900

    [feat] // 문제 4번 // 숫자 6개를 입력받아 내림차순으로 정렬 // [버블을 사용]

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV4으로 넘어간다.
+// 문제 4번 
+// 숫자 6개를 입력받아 내림차순으로 정렬합니다.
+// 입력 예시 : 3 5 1 6 5 8
+// 출력 예시 : 865531
+// 구체적인 예시 : 
+// 입력 : 4 2 9 7 1 5
+// 출력 : 97541
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int arr[6];
+
+	for(int i = 0 ; i < 6; ++i)
+		std::cin >> arr[i];
+
+	for(int i = 0 ; i < 6; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "정렬을 시작합니다..." << std::endl;
+
+	for(int i = 0 ; i <6; ++i)
+	{
+		for(int j = 0;  j < 6 -i -1; ++j)
+		{
+			if(arr[j] < arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+
+	std::cout << "정렬을 종료합니다..." << std::endl;
+
+	for(int i = 0 ; i < 6; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
