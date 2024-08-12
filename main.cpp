f5aed4ee13dde834cffc4d666276bf4b0391c253
SDW1014 <apif730@gmail.com>
2024-08-12 11:31:47 +0900

    [feat] // 문제 1번 // 변수 a, b에 숫자 2개 입력받기 및 a ~ b까지 출력 // TIP) WHILE를 사용

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV4으로 넘어간다.
+// 문제 1번 
+// 변수 a, b에 숫자 2개 입력받기 및 a ~ b까지 출력
+// TIP) WHILE를 사용
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b;
+
+	std::cin >> a >> b;
+
+	while(a <= b)
+	{
+		std::cout << a << std::endl;
+		a++;
+	}
 
 	return 0;
 }
