0700b293ae5e9ad7405e2bd0398c8f0ce2188abb
SDW1014 <apif730@gmail.com>
2024-08-12 16:21:22 +0900

    [feat] // 문제 13번 // char 변수 2개를 만들고 문자 2개를 입력 // 그리고 그 문자를 **가르키는** 포인터 2개 // 포인터만을 이용하여 두개의 char변수를 SWAP

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 
 // [2024-08-12]
-// 제목 : LV4으로 넘어간다.
+// 문제 13번
+// char 변수 2개를 만들고 문자 2개를 입력받아주세요
+// 그리고 그 문자를 **가르키는** 포인터 2개를 만들어주세요
+// 포인터만을 이용하여 두개의 char변수를 SWAP 하고 출력 해 주세요
+// 입력 예시: D A
+// 출력 예시: A D
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+
+	char a, b;
+
+	std::cin >> a >> b;
+
+	char* pA = &a;
+	char* pB = &b;
+
+	std::cout << *pA << " " << *pB << std::endl;
+
+	char temp = *pA;
+	*pA = *pB;
+	*pB = temp;
+
+	std::cout << *pA << " " << *pB << std::endl;
 
 	return 0;
 }
