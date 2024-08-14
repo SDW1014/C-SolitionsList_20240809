c58bede9a9f74fcb68b5d8835e423b20499844a4
SDW1014 <apif730@gmail.com>
2024-08-15 03:45:42 +0900

    [feat] // 문제 2번 // 소문자 하나를 입력 받아서 그 소문자부터 a까지 거꾸로 출력하는 프로그램 // 입력: 소문자 하나 // 출력: 입력한 소문자부터 a까지의 알파벳을 거꾸로 출력 // 예시: g를 입력 받으면 gfedcba 출력

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV6
+// 문제 2번
+// 소문자 하나를 입력 받아서 그 소문자부터 a까지 거꾸로 출력하는 프로그램
+// 입력: 소문자 하나
+// 출력: 입력한 소문자부터 a까지의 알파벳을 거꾸로 출력
+// 예시: g를 입력 받으면 gfedcba 출력
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char input;
+	std::cin >> input;
+
+	for(int i = input; i >= 'a'; --i)
+		std::cout << static_cast<char>(i) << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
