1f90deb2ba784d8999bddabb45054cbc792806ba
SDW1014 <apif730@gmail.com>
2024-08-15 03:41:22 +0900

    [feat] // 문제 13번 // 숫자 a b c세개를 입력 받아 주세요. // a 와 b 사이에 있는 숫자들을 c번 출력 해 주세요. // 입력 예시: 1 9 2 // 출력 예시: 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV6
+// 문제 13번
+// 숫자 a b c세개를 입력 받아 주세요.
+// a 와 b 사이에 있는 숫자들을 c번 출력 해 주세요.
+// 입력 예시: 1 9 2
+// 출력 예시: 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a, b, c;
+	std::cin >> a >> b >> c;
+
+	for(int i = 0 ; i < c; ++i)
+	{
+		for(int j = a; j <= b; ++j)
+			std::cout << j << " ";
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
