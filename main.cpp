03a6acdf16aa9b0206a76d26a09f1cfd11b748ab
SDW1014 <apif730@gmail.com>
2024-08-15 03:16:06 +0900

    [feat] // 문제 7번 // 숫자 하나를 입력 받아 주세요. // 3을 입력 받았다면 ###를 3번 출력 해 주세요. // 4를 입력 받았다면 ####를 4번 출력 해 주세요. // 꼭 2중 for문으로 작성 해 주세요.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV6
+// 문제 7번
+// 숫자 하나를 입력 받아 주세요.
+// 3을 입력 받았다면 ###를 3번 출력 해 주세요.
+// 4를 입력 받았다면 ####를 4번 출력 해 주세요.
+// 꼭 2중 for문으로 작성 해 주세요.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int num; 
+	std::cin >> num;
+
+	for(int i = 0 ; i < num; ++i)
+	{
+		for(int j = 0; j < num; ++j)
+		{
+			std::cout << "#";
+		}
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
