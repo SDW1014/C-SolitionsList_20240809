6b361cc51f6b9a97ef834669bd07d6c1b1594578
SDW1014 <apif730@gmail.com>
2024-08-15 03:02:20 +0900

    [feat] // 제목 : LV6 // 문제 3번 // 숫자 하나를 입력 받아 주세요. // 입력 받은 숫자 만큼 1 2 3 4 5 를 출력 해 주세요. // 4를 입력 받았다면 // 1 2 3 4 5 // 1 2 3 4 5 // 1 2 3 4 5 // 1 2 3 4 5 // 를 출력 해 주세요. // 2중 for문으로 코딩을 해 주세요.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 
 // [2024-08-15]
 // 제목 : LV6
+// 문제 3번
+// 숫자 하나를 입력 받아 주세요.
+// 입력 받은 숫자 만큼 1 2 3 4 5 를 출력 해 주세요.
+// 4를 입력 받았다면
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 1 2 3 4 5
+// 를 출력 해 주세요.
+// 2중 for문으로 코딩을 해 주세요.
+
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int num;
+	std::cin >> num;
+
+	for(int i = 0; i < num; ++i)
+	{
+		for(int j = 0; j < 5; ++j)
+			std::cout << j + 1 << " ";
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
