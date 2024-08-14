c3abfc75b7195f3a3e191dfdea2f431d4a386246
SDW1014 <apif730@gmail.com>
2024-08-15 03:08:29 +0900

    [feat] // 문제 4번 // 다음 char 배열을 하드 코딩 해 주세요 // | $ | @ | D | A | 9 | # | // 그리고 for문을 돌려 // 각 문자와 ASCII값을 출력 해 주세요

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV6
+// 문제 4번 
+// 다음 char 배열을 하드 코딩 해 주세요
+// | $ | @ | D | A | 9 | # |
+// 그리고 for문을 돌려
+// 각 문자와 ASCII값을 출력 해 주세요
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char arr[6] = {'$', '@', 'D', 'A', '9', '#'};
 
+	for(int i = 0; i < 6; ++i)
+		std::cout << arr[i] << " : " << static_cast<int>(arr[i]) << std::endl;
+ 
 	return 0;
 }
