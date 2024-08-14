c99bf4d95b18b2395399436000b2d157e0056847
SDW1014 <apif730@gmail.com>
2024-08-15 04:09:00 +0900

    [feat] // 문제 10번 // 대문자 3개를 배열에 입력받고, 첫번째 문자가 사전순으로 가장 뒤에 있는지 확인하는 프로그램 // 입력 예시: Q C C // 출력 예시: 옳다Q 또는 옳지않음

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV6
+// 문제 10번 
+// 대문자 3개를 배열에 입력받고, 첫번째 문자가 사전순으로 가장 뒤에 있는지 확인하는 프로그램
+// 입력 예시: Q C C
+// 출력 예시: 옳다Q 또는 옳지않음
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char arr[3] = {0};
+
+	for(int i = 0; i < 3; ++i)
+		std::cin >> arr[i];
+
+	if(arr[0] > arr[1] && arr[0] > arr[2])
+		std::cout << "옳다" << arr[0] << std::endl;
+	else
+		std::cout << "옳지않음" << std::endl;
 
 	return 0;
 }
