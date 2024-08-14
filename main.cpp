dfe9c5a584e4b1bcb2fe5c0bfed28f26b835f34a
SDW1014 <apif730@gmail.com>
2024-08-15 03:11:17 +0900

    [feat] // 제목 : LV6 // 문제 5번: 아래의 배열을 하드코딩 해 주세요. // 배열: B T K A // 정수형 변수(int)에 숫자 하나를 입력 받아 주세요. // 입력한 숫자 만큼 위의 배열을 출력 해 주세요. // 예시: 5를 입력하셨다면 // B T K A // B T K A // B T K A // B T K A // B T K A

--- main.cpp
+++ main.cpp
@@ -2,10 +2,30 @@
 
 // [2024-08-15]
 // 제목 : LV6
+// 문제 5번: 아래의 배열을 하드코딩 해 주세요.
+// 배열: B T K A
+// 정수형 변수(int)에 숫자 하나를 입력 받아 주세요.
+// 입력한 숫자 만큼 위의 배열을 출력 해 주세요.
+// 예시: 5를 입력하셨다면
+// B T K A
+// B T K A
+// B T K A
+// B T K A
+// B T K A
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char arr[4] = {'B', 'T', 'K', 'A'};
+
+	int num;
+	std::cin >> num;
+
+	for(int i = 0 ; i < num; ++i)
+	{
+		for(int j = 0; j < 4; ++j)
+			std::cout << arr[j] << " ";
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
