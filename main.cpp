dde72818e20eb77ac9eef55a02a54abe18311482
SDW1014 <apif730@gmail.com>
2024-08-15 03:29:32 +0900

    [feat] // 문제 10번 // 소문자를 입력받아 대문자로 변환하는 프로그램 // 입력받은 소문자를 대문자로 바꾸어 출력합니다. // 예를 들어, 'b'를 입력받으면 'B'가 출력됩니다. // 아스키 코드 규칙을 이용하여 변환할 수 있습니다.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV6
+// 문제 10번
+// 소문자를 입력받아 대문자로 변환하는 프로그램
+// 입력받은 소문자를 대문자로 바꾸어 출력합니다.
+// 예를 들어, 'b'를 입력받으면 'B'가 출력됩니다.
+// 아스키 코드 규칙을 이용하여 변환할 수 있습니다.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char input;
+	std::cin >> input;
+
+	if(input >= 'a' && input <= 'z')
+		std::cout << static_cast<char>(input - 32);
 
 	return 0;
 }
