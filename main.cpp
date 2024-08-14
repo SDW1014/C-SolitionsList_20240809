9876b8b15398c39bcbfad667c057156d81b9b757
SDW1014 <apif730@gmail.com>
2024-08-15 03:56:48 +0900

    [feat] // 문제 7번 // 대문자 또는 소문자 2개를 입력 받으세요. // 만약 대문자가 입력되면 소문자로 바꾸고, // 만약 소문자가 입력되면 대문자로 바꾸는 프로그램을 만들어 주세요. // 입력 예시: T R // 출력 예시: t r

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV6
+// 문제 7번
+// 대문자 또는 소문자 2개를 입력 받으세요.
+// 만약 대문자가 입력되면 소문자로 바꾸고,
+// 만약 소문자가 입력되면 대문자로 바꾸는 프로그램을 만들어 주세요.
+// 입력 예시: T R
+// 출력 예시: t r
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char input[2] = {0};
+	std::cin >> input[0] >> input[1];
+
+	if(input[0] >= 'A' && input[0] <= 'Z')
+		std::cout << static_cast<char>(input[0] + 32) << " ";
+	else
+		std::cout << static_cast<char>(input[0] - 32) << " ";
+
+	if(input[1] >= 'A' && input[1] <= 'Z')
+		std::cout << static_cast<char>(input[1] + 32) << std::endl;
+	else
+		std::cout << static_cast<char>(input[1] - 32) << std::endl;
 
 	return 0;
 }
