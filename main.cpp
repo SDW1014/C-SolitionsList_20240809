44184c67b1269a97b5983db34b86f543a9593e02
SDW1014 <apif730@gmail.com>
2024-08-15 19:16:37 +0900

    [feat] // 문제 4번
    // 1. 숫자 5개를 입력받을 배열 선언
    // 2. for문을 사용하여 배열에 숫자 입력
    // 3. 배열 안에서 3 ~ 7 사이의 숫자 카운팅
    // 4. 카운팅 결과 출력

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV7
+// 문제 4번
 
-// 2차원 배열
-// - [](대괄호)를 두 번 사용하여 선언
-// - 첫번째는 세로의 크기, 두번째는 가로의 크기
-// - 수학에서처럼 y,x로 표기하는 습관
+// 1. 숫자 5개를 입력받을 배열 선언
+// 2. for문을 사용하여 배열에 숫자 입력
+// 3. 배열 안에서 3 ~ 7 사이의 숫자 카운팅
+// 4. 카운팅 결과 출력
 
-// 나머지 연산자
-// - 홀짝 및 배수 구분
-
-// 카운팅
+// 입력 예시: 1 2 3 4 5
+// 출력 예시: 3
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int arr[5];
+	for(int i = 0; i < 5; ++i)
+		std::cin >> arr[i];
+
+
+	int count = 0;
+	std::cout << "숫자 5개 입력: ";
+	for(int i = 0; i < 5; ++i)
+	{
+		if(arr[i] >= 3 && arr[i] <= 7)
+			++count;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "3 ~ 7 사이의 숫자 개수: " << count << std::endl;
 
 	return 0;
 }
