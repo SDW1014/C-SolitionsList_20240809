21626d220078d9cd0bd564d0cbb38015242d9b17
SDW1014 <apif730@gmail.com>
2024-08-15 19:33:52 +0900

    [feat] // 문제 7번
    // 숫자 3개를 입력받아 가장 큰 값과 가장 작은 값을 출력하는 프로그램
    // - MAX값 출력: a, b, c 중 가장 큰 값
    // - MIN값 출력: a, b, c 중 가장 작은 값
    // - 조건문을 사용하여 최대값과 최소값을 결정
    
    // 입력 예시: 5 3 3
    // 출력 예시: MAX=5, MIN=3

--- main.cpp
+++ main.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV7
+// 문제 7번
 
-// 2차원 배열
-// - [](대괄호)를 두 번 사용하여 선언
-// - 첫번째는 세로의 크기, 두번째는 가로의 크기
-// - 수학에서처럼 y,x로 표기하는 습관
+// 숫자 3개를 입력받아 가장 큰 값과 가장 작은 값을 출력하는 프로그램
+// - MAX값 출력: a, b, c 중 가장 큰 값
+// - MIN값 출력: a, b, c 중 가장 작은 값
+// - 조건문을 사용하여 최대값과 최소값을 결정
 
-// 나머지 연산자
-// - 홀짝 및 배수 구분
+// 입력 예시: 5 3 3
+// 출력 예시: MAX=5, MIN=3
 
-// 카운팅
+void bubbleSort(int arr[])
+{
+	for(int i = 0 ; i < 3; ++i)
+	{
+		for(int j = 0; j < 3-i; ++j)
+		{
+			if(arr[j] < arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int arr[3];
+	for(int i = 0 ; i < 3; ++i)
+		std::cin >> arr[i];
+
+	bubbleSort(arr);
+
+	for(int i = 0; i < 3; ++i)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "MAX=" << arr[0] << ", MIN=" << arr[2] << std::endl;
 
 	return 0;
 }
