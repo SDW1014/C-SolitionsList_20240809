fb03e67ffef5ecee76dda3ffebc5ade07a5601c3
SDW1014 <apif730@gmail.com>
2024-08-15 19:48:46 +0900

    [feat] // 문제 10번 // 문자 1개를 입력받고 // input함수에서  4 x 4 배열에 입력받은 문자를 꽉 채워주세요 // output함수에서 4 x 4 배열값을 출력 해 주세요 // 1. ex) 만약 t를 입력받았다면 이렇게 채우고 출력하시면 됩니다.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 
 // [2024-08-15]
-// 제목 : LV7
+// 문제 10번
+// 문자 1개를 입력받고
+// input함수에서  4 x 4 배열에 입력받은 문자를 꽉 채워주세요
+// output함수에서 4 x 4 배열값을 출력 해 주세요
+// 1. ex) 만약 t를 입력받았다면 이렇게 채우고 출력하시면 됩니다.
+// | t | t | t | t |
+// |---|---|---|---|
+// | t | t | t | t |
+// | t | t | t | t |
+// | t | t | t | t |
 
-// 2차원 배열
-// - [](대괄호)를 두 번 사용하여 선언
-// - 첫번째는 세로의 크기, 두번째는 가로의 크기
-// - 수학에서처럼 y,x로 표기하는 습관
+// ## 입력 예시
+// a
 
-// 나머지 연산자
-// - 홀짝 및 배수 구분
-
-// 카운팅
+// ## 출력 예시
+// aaaa
+// aaaa
+// aaaa
+// aaaa
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char input;
+	std::cin >> input;
+
+	char arr[4][4] = {};
+
+	for(int i = 0; i < 4; ++i)
+		for(int j = 0; j < 4; ++j)
+			arr[i][j] = input;
+
+	for(int i = 0; i < 4; ++i)
+	{
+		for(int j = 0; j < 4; ++j)
+			std::cout << arr[i][j];
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
