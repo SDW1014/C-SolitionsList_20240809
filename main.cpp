174500560cb9c9a302e3fc4a033226a4b51483c2
SDW1014 <apif730@gmail.com>
2024-08-16 14:13:00 +0900

    [feat] // 문제 1번 // 1. 사용자로부터 숫자 n을 입력받는다. // 2. 1부터 n까지의 숫자를 출력해야 한다. // 3. while문을 사용하여 반복문을 구현한다. // 4. 출력 형식은 공백으로 구분된 숫자들이다.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
 // [2024-08-16]
-// 제목 : LV8
+// 제목 : 연습문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
-
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
+// 문제 1번
+// 1. 사용자로부터 숫자 n을 입력받는다.
+// 2. 1부터 n까지의 숫자를 출력해야 한다.
+// 3. while문을 사용하여 반복문을 구현한다.
+// 4. 출력 형식은 공백으로 구분된 숫자들이다.
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int n;
+	std::cin >> n;
+
+	int index = 1;
+	while(true)
+	{
+		std::cout << index << " ";
+		index++;
+
+		if(n < index) 
+			break;
+	}
 
 	return 0;
 }
