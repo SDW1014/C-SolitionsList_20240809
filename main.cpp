2ce349113347cecf1a897af872aafd35e2e0934e
SDW1014 <apif730@gmail.com>
2024-08-16 16:14:14 +0900

    [feat] // 문제 11번 분석 // 1. 커피 가격을 입력받는다. // 2. 입력된 가격이 3500 이상 5000 이하일 경우 starBox() 함수를 호출한다. // 3. 입력된 가격이 2500 이상 3500 미만일 경우 macDoll() 함수를 호출한다. // 4. 위 조건에 해당하지 않으면 copyBean() 함수를 호출한다. // // 각 함수의 역할: // - starBox() : 1부터 20 사이의 홀수를 출력하는 함수 (for문 사용) // - macDoll() : H부터 A까지 거꾸로 출력하는 함수 (for문 사용) // - copyBean() : -5부터 5까지 숫자를 출력하는 함수 (for문 사용)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 
 // [2024-08-16]
-// 제목 : LV8
+// 제목 : 연습문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
+// 문제 11번 분석
+// 1. 커피 가격을 입력받는다.
+// 2. 입력된 가격이 3500 이상 5000 이하일 경우 starBox() 함수를 호출한다.
+// 3. 입력된 가격이 2500 이상 3500 미만일 경우 macDoll() 함수를 호출한다.
+// 4. 위 조건에 해당하지 않으면 copyBean() 함수를 호출한다.
+//
+// 각 함수의 역할:
+// - starBox() : 1부터 20 사이의 홀수를 출력하는 함수 (for문 사용)
+// - macDoll() : H부터 A까지 거꾸로 출력하는 함수 (for문 사용)
+// - copyBean() : -5부터 5까지 숫자를 출력하는 함수 (for문 사용)
 
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
+void starBox()
+{
+	for(int i = 1; i <= 20; ++i)
+		std::cout << i << " ";
+}
 
+void macDoll()
+{
+	for(char i = 'H'; i >= 'A'; --i)
+		std::cout << static_cast<char>(i) << " ";
+}
+
+void copyBean()
+{
+	for(int i = -5; i <= 5; ++i)
+		std::cout << i << " ";
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int price;
+	std::cin >> price;
+
+	if(price >= 3500 && price <= 5000)
+		starBox();
+	else if(price >= 2500 && price < 3500)
+		macDoll();
+	else
+		copyBean();
 
 	return 0;
 }
