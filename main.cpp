96dd53a0342510f6efbb0ba025e8394cb9c61c59
SDW1014 <apif730@gmail.com>
2024-08-16 15:24:15 +0900

    [feat] // 문제 3번 // 1. input 함수와 output 함수를 만들어야 한다. // 2. input 함수에서는 두 개의 숫자를 전역 변수에 입력받는다. // 3. output 함수에서는 5부터 입력받은 두 수의 합까지 while문을 사용하여 출력한다. // 4. 입력 예시: 3 4 // 5. 출력 예시: 5 6 7

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 
 // [2024-08-16]
-// 제목 : LV8
+// 제목 : 연습문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
+// 문제 3번
+// 1. input 함수와 output 함수를 만들어야 한다.
+// 2. input 함수에서는 두 개의 숫자를 전역 변수에 입력받는다.
+// 3. output 함수에서는 5부터 입력받은 두 수의 합까지 while문을 사용하여 출력한다.
+// 4. 입력 예시: 3 4
+// 5. 출력 예시: 5 6 7
 
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
+int a, b;
 
-
+void input()
+{
+	std::cin >> a >> b;
+}
+void output()
+{
+	int index = 5;
+	while(index <= a + b)
+	{
+		std::cout << index << " ";
+		index++;
+	}
+}
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	input();
+	output();
 
 	return 0;
 }
