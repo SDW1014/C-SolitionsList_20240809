3399d4606b15f98737abb2ede86c33ed16c25e7f
SDW1014 <apif730@gmail.com>
2024-08-16 15:39:34 +0900

    [feat] // 문제 6번 // 1. 전역 배열로 3 x 4 크기의 배열을 생성해야 한다. // 2. input 함수에서 숫자 1개를 입력받고, 그 숫자부터 1씩 증가하는 값으로 배열을 채운다. //    예를 들어, 입력받은 숫자가 2라면 배열은 다음과 같이 채워진다: //    | 2 | 3 | 4 | 5 | //    |---|---|---|---| //    | 6 | 7 | 8 | 9 | //    |10 |11 |12 |13 | // 3. process 함수에서 배열의 모든 요소에 1씩 더해야 한다. // 4. output 함수에서 배열의 모든 값을 출력해야 한다.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
 
 // [2024-08-16]
-// 제목 : LV8
+// 제목 : 연습문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
+// 문제 6번
+// 1. 전역 배열로 3 x 4 크기의 배열을 생성해야 한다.
+// 2. input 함수에서 숫자 1개를 입력받고, 그 숫자부터 1씩 증가하는 값으로 배열을 채운다.
+//    예를 들어, 입력받은 숫자가 2라면 배열은 다음과 같이 채워진다:
+//    | 2 | 3 | 4 | 5 |
+//    |---|---|---|---|
+//    | 6 | 7 | 8 | 9 |
+//    |10 |11 |12 |13 |
+// 3. process 함수에서 배열의 모든 요소에 1씩 더해야 한다.
+// 4. output 함수에서 배열의 모든 값을 출력해야 한다.
 
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
+int arr[3][4];
 
+void input()
+{
+	int inputNum;
+	std::cin >> inputNum;
+
+	for(int i = 0; i < 3; ++i)
+		for(int j = 0; j < 4; ++j)
+			arr[i][j] = inputNum + (i * 4) + j;
+}
+
+void process()
+{
+	for (int i = 0; i < 3; ++i)
+		for (int j = 0; j < 4; ++j)
+			arr[i][j] = arr[i][j] + 1;
+}
+
+void output()
+{
+	for(int i = 0; i < 3; ++i)
+	{
+		for(int j = 0; j < 4; ++j)
+			std::cout << arr[i][j] << " ";
+		std::cout << std::endl;
+	}
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	input();
+	output();
+	process();
+	output();
 
 	return 0;
 }
