becddc3471244cbb89ce28a00a6904d21a8ea62b
SDW1014 <apif730@gmail.com>
2024-08-16 14:16:29 +0900

    [feat] // 문제 2번 // 1. 숫자 6개를 배열에 입력 받기 // 2. 그리고 for문을 이용하여 0번 index 부터 5번 index까지 // 3. 배열 안에 있는 값을 하나씩 출력합니다. // 4. 만약 출력할 값이 7이 나온다면 중단(break)를 시킵니다.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 
 // [2024-08-16]
-// 제목 : LV8
+// 제목 : 연습문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
-
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
-
+// 문제 2번
+// 1. 숫자 6개를 배열에 입력 받기
+// 2. 그리고 for문을 이용하여 0번 index 부터 5번 index까지
+// 3. 배열 안에 있는 값을 하나씩 출력합니다.
+// 4. 만약 출력할 값이 7이 나온다면 중단(break)를 시킵니다.
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int arr[6];
+
+	for(int i = 0; i < 6; ++i)
+		std::cin >> arr[i];
+
+	for(int i = 0; i < 6; ++i)
+	{
+		if(arr[i] == 7)
+			break;
+		std::cout << arr[i] << " ";
+	}
 
 	return 0;
 }
