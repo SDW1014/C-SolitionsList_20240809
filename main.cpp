5fa27f69196d007d2f776d7e0e908b7d059e2f76
SDW1014 <apif730@gmail.com>
2024-08-16 16:29:38 +0900

    [feat] // 문제 2번 // 놀이동산에 친구들이 줄 서 있습니다. (위 배열을 하드코딩 해 주세요) // 친구의 이니셜 문자 하나를 입력받고, 그 친구가 몇 번째 Index에서 기다리고 있는지 출력 해 주세요.

--- main.cpp
+++ main.cpp
@@ -3,19 +3,28 @@
 // [2024-08-16]
 // 제목 : LV8 - 복습 문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
-
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
+// 문제 2번
+// 놀이동산에 친구들이 줄 서 있습니다. (위 배열을 하드코딩 해 주세요)
+// 친구의 이니셜 문자 하나를 입력받고, 그 친구가 몇 번째 Index에서 기다리고 있는지 출력 해 주세요.
+// 1. ex) A를 입력받았다면 "**2번 INDEX"**를 출력하면 됩니다.
+// 입력 예시: B
+// 출력 예시: 3번 INDEX
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char friends[6] = {'D', 'T', 'A', 'B', 'W', 'Q'};
+
+	char friendInitial = ' ';
+
+	std::cin >> friendInitial;
+
+	for(int i = 0; i < 6; ++i)
+		if(friends[i] == friendInitial)
+		{
+			std::cout << i + 1 << "번 INDEX" << std::endl;
+			break;
+		}
 
 	return 0;
 }
