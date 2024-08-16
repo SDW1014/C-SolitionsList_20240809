a061abeba26cb02d29d6af8a87bc1a9b60c8e70e
SDW1014 <apif730@gmail.com>
2024-08-16 15:48:32 +0900

    [feat] // 문제 9번 // 2차배열 2 x 3 사이즈로 선언 해주세요 // input함수에서 각 배열에 숫자를 입력받아주세요 // process함수에서 각 배열의 요소들의 합을 구해주세요 (sum구하기) // output함수에서 합을 출력 해 주세요 // 예시: 만약 1 2 3 4 5 6을 입력하였다면 // | 1 | 2 | 3 | // |---|---|---| // | 4 | 5 | 6 | // input함수에서 배열에 값을 채워주세요 // process에서 sum을 구한 후 // output에서 합을 출력 해 주세요 // 출력결과 : 21

--- main.cpp
+++ main.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 
 // [2024-08-16]
-// 제목 : LV8
+// 제목 : 연습문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
+// 문제 9번
+// 2차배열 2 x 3 사이즈로 선언 해주세요
+// input함수에서 각 배열에 숫자를 입력받아주세요
+// process함수에서 각 배열의 요소들의 합을 구해주세요 (sum구하기)
+// output함수에서 합을 출력 해 주세요
+// 예시: 만약 1 2 3 4 5 6을 입력하였다면
+// | 1 | 2 | 3 |
+// |---|---|---|
+// | 4 | 5 | 6 |
+// input함수에서 배열에 값을 채워주세요
+// process에서 sum을 구한 후
+// output에서 합을 출력 해 주세요
+// 출력결과 : 21
 
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
+void input(int arr[2][3])
+{
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 3; j++)
+			std::cin >> arr[i][j];
+}
 
+void process(int arr[2][3], int &sum)
+{
+	for(int i = 0; i < 2; ++i)
+		for(int j = 0; j < 3; ++j)
+			sum += arr[i][j];
+}
+
+void output(int &sum)
+{
+	std::cout << sum << std::endl;
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int arr[2][3] = {0};
+	int result = 0;
+
+	input(arr);
+	process(arr, result);
+	output(result);
 
 	return 0;
 }
