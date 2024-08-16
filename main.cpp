7c8eae7a95a799699432d5c40a0b52d52f9da085
SDW1014 <apif730@gmail.com>
2024-08-16 16:56:05 +0900

    [feat] // 문제 9번 // 1. 두 개의 문자를 입력받는다. // 2. 2차원 배열에 입력받은 문자로 채운다. // 3. for문을 사용하여 전체 배열을 출력한다. // 4. 예시: A G를 입력받으면 2차원 배열은 다음과 같다: //    | A | A | A | A | G | G | //    | A | A | A | A | G | G | //    | A | A | A | A | G | G |

--- main.cpp
+++ main.cpp
@@ -3,19 +3,38 @@
 // [2024-08-16]
 // 제목 : LV8 - 복습 문제
 
-// while문
-// 1. for문과 차이는 없다. 
-// 2. 대신 while구조가 더 단순하다. 
-// 3. 꼭 반드시 써야한다? 는 아니다. 
-
-// break문
-// 1. 반복문을 빠져나가는 역할을 한다. 
-// 2. 2중 while문도 똑같이 사용가능
+// 문제 9번
+// 1. 두 개의 문자를 입력받는다.
+// 2. 2차원 배열에 입력받은 문자로 채운다.
+// 3. for문을 사용하여 전체 배열을 출력한다.
+// 4. 예시: A G를 입력받으면 2차원 배열은 다음과 같다:
+//    | A | A | A | A | G | G |
+//    | A | A | A | A | G | G |
+//    | A | A | A | A | G | G |
 
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	char arr[3][6] = {0};
+	char a, b;
 
+	std::cout << "두 개의 문자를 입력하세요: ";
+	std::cin >> a >> b;
+	
+	for(int i = 0; i < 3; ++i)
+		for(int j = 0; j < 6; ++j)
+		{
+			if(j <= 3)
+				arr[i][j] = a;
+			else
+				arr[i][j] = b;
+		}
+
+	for(int i = 0; i < 3; ++i)
+	{
+		for(int j = 0; j < 6; ++j)
+			std::cout << arr[i][j] << " ";
+		std::cout << std::endl;
+	}
 	return 0;
 }
