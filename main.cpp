c54fd9a66ccb485fb6099c849630211fd282d7bd
SDW1014 <apif730@gmail.com>
2024-08-17 19:12:53 +0900

    [feat] LV10,LV11의 연습문제

--- main.cpp
+++ main.cpp
@@ -1,28 +1,11 @@
 #include <iostream>
 
 // [2024-08-17]
-// 제목 : LV11 - 최대값, 최소값 찾기
+// 제목 : LV10,LV11 - 연습문제
 
 int main()
 {
-	int arr[10] = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 };
-	int max = 0;
-
-	for(size_t i = 0; i < 8; ++i)
-	{
-		if(max < arr[i])
-			max = arr[i];
-	}
-
-	int min = 987654321;
-	for(size_t i = 0; i < 8; ++i)
-	{
-		if(min > arr[i])
-			min = arr[i];
-	}
-
-	std::cout << "최대값 : " << max << std::endl;
-	std::cout << "최소값 : " << min << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
 }
