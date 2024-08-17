82c5ba8ccf2fc2a0bddb2968fc7a2b9886ce0581
SDW1014 <apif730@gmail.com>
2024-08-17 18:36:31 +0900

    [feat] LV10 - Return 키워드

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
-// [2024-08-16]
-// 제목 : LV10
+// [2024-08-17]
+// 제목 : LV10 - Return 키워드
 
+int Add(int a, int b)
+{
+	return a + b;
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int a = 5; 
+	int b = 6;
+
+	int result = Add(a, b);
+
+	std::cout << result << std::endl;
 
 	return 0;
 }
