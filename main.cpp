2f970cdebe045f34558eec9aabbcfb6d1c2ddfa2
SDW1014 <apif730@gmail.com>
2024-08-17 18:51:16 +0900

    [feat]// 제목 : LV11 - 변수의 자료형 일치 // 1. 주소값을 저장하는 용도로 void* 를 사용 // 2. 해당 주소로 이동했을 떄 알맞은 크기 만큼 접근하기 위해서 포이넡 변수의 자료형과 실제 변수의 자료형을 맞춰야 한다.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
 // [2024-08-17]
-// 제목 : LV11 - 기본적인 사용과 16진수의 주소값
-// 1. void* : 보통 주소를 저장하기 위해서 사용한다. 
-// 2. 주소값은 16진수로 표현한다. (0x00000000)
+// 제목 : LV11 - 변수의 자료형 일치
+// 1. 주소값을 저장하는 용도로 void* 를 사용
+// 2. 해당 주소로 이동했을 떄 알맞은 크기 만큼 접근하기 위해서 포이넡 변수의 자료형과 실제 변수의 자료형을 맞춰야 한다.
 
 int main()
 {
 	int number = 0;
+
 	void* p = &number;
 
-	std::cout << &number << std::endl;
+	int* numberCopy = &number;
+	*numberCopy = 100;
+
+	char ch = 'A';
+	char* pCh = &ch;
 
 	return 0;
 }
