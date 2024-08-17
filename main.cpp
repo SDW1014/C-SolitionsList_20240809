d259f7979c6fd0d1c5688e18c8b9f09ae876c343
SDW1014 <apif730@gmail.com>
2024-08-17 19:11:51 +0900

    [feat] 설명 : LV11 - 최대값, 최소값 찾기

--- main.cpp
+++ main.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 
 // [2024-08-17]
-// 제목 : LV11 - 플래그 코딩 기법
-// 1. 뭐 거창한게 있는건 아니고, 플래그 세워놓고 체크하는 그런 기법을 의미함
-
+// 제목 : LV11 - 최대값, 최소값 찾기
 
 int main()
 {
 	int arr[10] = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3 };
-	int flag = 0;
+	int max = 0;
 
 	for(size_t i = 0; i < 8; ++i)
 	{
-		if(arr[i] == 3)
-		{
-			flag = 1;
-			break;
-		}
+		if(max < arr[i])
+			max = arr[i];
 	}
 
-	if(flag == 1)
-		std::cout << "3이 있습니다." << std::endl;
-	else
-		std::cout << "3이 없습니다." << std::endl;
+	int min = 987654321;
+	for(size_t i = 0; i < 8; ++i)
+	{
+		if(min > arr[i])
+			min = arr[i];
+	}
 
-	
+	std::cout << "최대값 : " << max << std::endl;
+	std::cout << "최소값 : " << min << std::endl;
 
 	return 0;
 }
