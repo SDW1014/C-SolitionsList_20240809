dde1e5b3f2975de2caf98f6f845e3027faa436d2
SDW1014 <apif730@gmail.com>
2024-08-17 18:43:59 +0900

    [feat] LV10 - 숫자 채우기 연습 [정사각형 배열]

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
 // [2024-08-17]
-// 제목 : LV10 - Return 키워드
-
-int Add(int a, int b)
-{
-	return a + b;
-}
+// 제목 : LV10 - 숫자 채우기 연습 [정사각형 배열]
 
 int main()
 {
-	int a = 5; 
-	int b = 6;
+	int arr[3][3] = {0};
 
-	int result = Add(a, b);
+	int x, y;
 
-	std::cout << result << std::endl;
+	int t = 1;
+
+	for(y = 0; y < 3; ++y)
+		for(x = 0; x < 3; ++x)
+		{
+			arr[y][x] = t;
+			t++;
+		}
+
+	for(y = 0; y < 3; ++y)
+	{
+		for(x = 0; x < 3; ++x)
+			std::cout << arr[y][x] << " ";
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
