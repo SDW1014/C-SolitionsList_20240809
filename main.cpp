ec78d5123e48ee0cce7bdcb9777b6769cc4c3142
SDW1014 <apif730@gmail.com>
2024-08-19 00:11:26 +0900

    [feat] 설명 : LV14 - 버블정렬, 선택정렬 (이중 While문)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
-// [2024-08-18]
-// 제목 : LV14 - 버블정렬, 선택정렬
+// [2024-08-19]
+// 제목 : LV14 - 버블정렬, 선택정렬 (이중 While문)
 
+// 이중 While문
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	int y = 0;
+	while(y < 10)
+	{
+		std::cout << "Hello World!" << std::endl;
+		int x = 0;
+		while(x < 10)
+		{
+			std::cout << "Hello World!" << std::endl;
+			x++;
+		}
+		y++;
+	}
 	return 0;
 }
