26e31303105fa9439c24d54ce1ab0c7220a1ef8f
SDW1014 <apif730@gmail.com>
2024-08-18 16:00:40 +0900

    [feat] 연습문제 - 문제 2번

--- main.cpp
+++ main.cpp
@@ -3,42 +3,28 @@
 // [2024-08-18]
 // 제목 : LV12 - 연습문제
 
+// 문제 2번
+// 두 개의 char 배열을 선언합니다.
+// str1은 최대 4글자 + NULL 문자, str2는 최대 3글자 + NULL 문자입니다.
+// 사용자로부터 문자열 입력을 받습니다.
+// 각 문자열의 길이를 계산합니다.
+// 문자열의 길이를 출력합니다.
+
+int getArrLength(char arr[])
+{
+	for(size_t i = 0; i < sizeof(arr)/sizeof(arr[0]); ++i)
+		if(arr[i] == '\0')
+			return i;
+	return -1;
+}
+
 int main()
 {
-	int arr[3][3] = {};
-	int num = 1;
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-		{
-			arr[y][x] = num;
-			num++;
-		}
-	}
+	char str1[5], str2[4];
+	std::cin >> str1 >> str2;
 
-	int arr2[3][3] = {};
-	int num2 = 1;
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 2-y; x < 3; ++x)
-		{
-			arr2[y][x] = num2;
-			num2++;
-		}
-	}
+	std::cout << "str1의 길이: " << getArrLength(str1) << std::endl;
+	std::cout << "str2의 길이: " << getArrLength(str2) << std::endl;
 
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-			std::cout << arr[y][x] << " ";
-		std::cout << std::endl;
-	}
-
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-			std::cout << arr2[y][x] << " ";
-		std::cout << std::endl;
-	}
 	return 0;
 }
