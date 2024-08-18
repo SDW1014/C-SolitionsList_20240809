96bb8dc78588cd28fd752d9c4a56a83290443023
SDW1014 <apif730@gmail.com>
2024-08-19 00:22:55 +0900

    [feat] LV14 - 연습문제

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 
 // [2024-08-19]
-// 제목 : LV14 - 버블정렬, 선택정렬 (여러문장 입력받기)
+// 제목 : LV14 - 연습문제
 
-// 여러문장 입력받기
 int main()
 {
-	char str[3][256] = {};
-
-	std::cin >> str[0];
-
-	std::cin >> str[1];
-
-	std::cin >> str[2];
+	std::cout << "Hello World" << std::endl;
 	return 0;
 }
