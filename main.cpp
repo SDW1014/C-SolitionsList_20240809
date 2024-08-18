640a3aa0e33a02b0b51a23435ebeff859cc74c56
SDW1014 <apif730@gmail.com>
2024-08-19 00:53:59 +0900

    [feat] 연습문제 - 문제 8번

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 // [2024-08-19]
 // 제목 : LV14 - 연습문제
 
+// 문제 8번
+// - 세 문장을 저장할 수 있는 3 x 10 배열을 생성
+// - 사용자로부터 세 문장을 입력받음
+// - CountLine 함수를 호출하여 문장 배열을 전달
+// - CountLine 함수는 각 문장의 길이를 계산하고 출력
+
+void CountLine(char (*arr)[10])
+{
+	for(int i = 0; i < 3; ++i)
+	{
+		int length = strlen(arr[i]);
+		std::cout << length << "=" << arr[i] << std::endl;
+	}
+}
+
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	char sentences[3][10];
+	for(int i = 0; i < 3; ++i)
+		std::cin >> sentences[i];
+
+	CountLine(sentences);
+
 	return 0;
 }
