c5b6a9e4473ecc3684f04f08c795f9083977a371
SDW1014 <apif730@gmail.com>
2024-08-18 16:50:26 +0900

    [feat] 연습문제 - 문제 7번

--- main.cpp
+++ main.cpp
@@ -3,42 +3,34 @@
 // [2024-08-18]
 // 제목 : LV12 - 연습문제
 
+// 문제 7번
+// 1. 문자열을 입력받는다. 최대 10글자.
+// 2. 추가로 문자 3개를 입력받는다.
+// 3. 각 문자가 문자열에 몇 번 존재하는지 세어서 출력한다.
+// 4. 문자열의 길이를 구할 때 break 문을 사용해야 한다.
+
+int count(char* str, char target)
+{
+	int count = -1;
+	for(int i = 0; i < strlen(str); ++i)
+		if(str[i] == target)
+			count++;
+	return count;
+}
+
 int main()
 {
-	int arr[3][3] = {};
-	int num = 1;
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-		{
-			arr[y][x] = num;
-			num++;
-		}
-	}
+	char str[256];
+	std::cout << "문자열을 입력하세요: ";
+	std::cin >> str;
 
-	int arr2[3][3] = {};
-	int num2 = 1;
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 2-y; x < 3; ++x)
-		{
-			arr2[y][x] = num2;
-			num2++;
-		}
-	}
+	char a, b, c;
+	std::cout << "문자 3개를 입력하세요: ";
+	std::cin >> a >> b >> c;
 
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-			std::cout << arr[y][x] << " ";
-		std::cout << std::endl;
-	}
+	std::cout << a << ": " << count(str, a) << std::endl;
+	std::cout << b << ": " << count(str, b) << std::endl;
+	std::cout << c << ": " << count(str, c) << std::endl;
 
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-			std::cout << arr2[y][x] << " ";
-		std::cout << std::endl;
-	}
 	return 0;
 }
