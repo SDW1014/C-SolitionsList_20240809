a358ccfb1c8ce78a2e5364294f3307a997f7808a
SDW1014 <apif730@gmail.com>
2024-08-18 22:04:14 +0900

    [feat] 설명 : LV13 - 포인터의 활용 (2차원 배열에서 최소,최대값 위치 기억하기)

--- main.cpp
+++ main.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 
 // [2024-08-18]
-// 제목 : LV13 - 포인터의 활용 (최대값 최소값 찾기)
+// 제목 : LV13 - 포인터의 활용 (2차원 배열에서 최소,최대값 위치 기억하기)
 
-// 최대값 최소값 찾기
-#include <map>
+// 2차원 배열에서 최소,최대값 위치 기억하기
+
+int vect[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
 int main()
 {
-	int vec[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int max = 0;
 	int min = 987654321;
+	int dy = 0;
+	int dx = 0;
 
-	int maxIdx = -1;
-	int minIdx = -1;
-
-	for(int i = 0; i < 10; ++i)
+	for(int y = 0; y < 3; ++y)
 	{
-		if(max < vec[i])
+		for(int x = 0; x < 3; ++x)
 		{
-			max = vec[i];
-			maxIdx = i;
-		}
-		if(min > vec[i])
-		{
-			min = vec[i];
-			minIdx = i;
+			if(vect[y][x] < min)
+			{
+				min = vect[y][x];
+				dy = y;
+				dx = x;
+			}
 		}
 	}
 	return 0;
