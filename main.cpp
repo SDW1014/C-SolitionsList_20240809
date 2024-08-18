d9dd4211436246a34349b5141a0d9a9de0ccf75a
SDW1014 <apif730@gmail.com>
2024-08-19 00:14:44 +0900

    [feat] 설명 : LV14 - 버블정렬, 선택정렬 (버블정렬)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 
 // [2024-08-19]
-// 제목 : LV14 - 버블정렬, 선택정렬 (이중 While문)
+// 제목 : LV14 - 버블정렬, 선택정렬 (버블정렬)
 
-// 이중 While문
+// 버블정렬
 int main()
 {
-	int y = 0;
-	while(y < 10)
+	int arr[6] = { 5,3,6,2,1,8 };
+
+	for(int i = 0; i < 6; ++i)
 	{
-		std::cout << "Hello World!" << std::endl;
-		int x = 0;
-		while(x < 10)
-		{
-			std::cout << "Hello World!" << std::endl;
-			x++;
-		}
-		y++;
+		std::cout << arr[i] << " ";
 	}
+	std::cout << std::endl;
+
+	for(int i = 0; i < 6; ++i)
+	{
+		for(int j = 0; j < 6 - i - 1; ++j)
+		{
+			if(arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	for(int i = 0; i < 6; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
 	return 0;
 }
