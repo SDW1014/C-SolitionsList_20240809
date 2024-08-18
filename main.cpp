0e01665ee0af48833ef3b62ee26f1f7ca58d86e7
SDW1014 <apif730@gmail.com>
2024-08-18 15:51:07 +0900

    [feat] 설명 : LV12 - 문자열 (2중 for문과 2차원 배열)

--- main.cpp
+++ main.cpp
@@ -8,32 +8,43 @@
 // 문자열 : 문자들의 집합을 뜻하는 의미다. ("ABC", "DEF", "GHI")
 // char str[5] : 는 4개의 글자 까지만 저장 할 수 있고 마지막에는 NULL 문자('\0')가 자동으로 추가된다.
 
-// 1.2 : 2차원 배열을 사용해 여러문장을 사용 하는 예
+// 1.2 : 2중 for문과 2차원 배열
 int main()
 {
-	// 2차원 배열 선언
-	char str2D[3][5] = 
+	int arr[3][3] = {};
+	int num = 1;
+	for(size_t y = 0; y < 3; ++y)
 	{
-		"ABC",
-		"DEF",
-		"HIJ"
-	};
+		for(size_t x = 0; x < 3; ++x)
+		{
+			arr[y][x] = num;
+			num++;
+		}
+	}
 
-	// 2차원 배열 출력
-	for(int i = 0; i < 3; ++i)
-		std::cout << str2D[i] << std::endl;
+	int arr2[3][3] = {};
+	int num2 = 1;
+	for(size_t y = 0; y < 3; ++y)
+	{
+		for(size_t x = 2-y; x < 3; ++x)
+		{
+			arr2[y][x] = num2;
+			num2++;
+		}
+	}
 
-	// 한글자씩 출력
-	for(int i = 0; i < 3; ++i)
-		for(int j = 0; j < 5; ++j)
-			std::cout << str2D[i][j] << std::endl;
+	for(size_t y = 0; y < 3; ++y)
+	{
+		for(size_t x = 0; x < 3; ++x)
+			std::cout << arr[y][x] << " ";
+		std::cout << std::endl;
+	}
 
-	// 문장을 입력
-	std::cout << "문장을 입력하세요 : ";
-	std::cin >> str2D[0];
-
-	// 주소값 출력
-	std::cout << str2D << std::endl;
-
+	for(size_t y = 0; y < 3; ++y)
+	{
+		for(size_t x = 0; x < 3; ++x)
+			std::cout << arr2[y][x] << " ";
+		std::cout << std::endl;
+	}
 	return 0;
 }
