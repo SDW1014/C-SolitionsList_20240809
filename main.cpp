dfd1625b022367024258f15276e7d12fb2221639
SDW1014 <apif730@gmail.com>
2024-08-18 17:26:23 +0900

    feat] LV12 - 복습문제

--- main.cpp
+++ main.cpp
@@ -1,44 +1,10 @@
 #include <iostream>
 
 // [2024-08-18]
-// 제목 : LV12 - 연습문제
+// 제목 : LV12 - 복습문제
 
 int main()
 {
-	int arr[3][3] = {};
-	int num = 1;
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-		{
-			arr[y][x] = num;
-			num++;
-		}
-	}
-
-	int arr2[3][3] = {};
-	int num2 = 1;
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 2-y; x < 3; ++x)
-		{
-			arr2[y][x] = num2;
-			num2++;
-		}
-	}
-
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-			std::cout << arr[y][x] << " ";
-		std::cout << std::endl;
-	}
-
-	for(size_t y = 0; y < 3; ++y)
-	{
-		for(size_t x = 0; x < 3; ++x)
-			std::cout << arr2[y][x] << " ";
-		std::cout << std::endl;
-	}
+	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
